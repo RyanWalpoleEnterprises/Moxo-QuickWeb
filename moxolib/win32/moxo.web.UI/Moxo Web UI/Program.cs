28200e6785cc81d811015ca821066aa5693289d0
Ryan Walpole <ryan.walpole@outlook.com>
2023-02-23 13:11:22 +1100

    Create Program.cs  (moxo.web.UI Template)
    Create the program startpoint for the moxo web UI template.
    
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

---
+++ moxolib/win32/moxo.web.UI/Moxo Web UI/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Moxo_Web_UI
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            try
+            {
+                string WV = CoreWebView2Environment.GetAvailableBrowserVersionString();
+
+                if (WV != null)
+                {
+                    Application.Run(new Startup());
+                }
+                else
+                {
+                    //Microsoft Edge WebView2 Runtime Not Installed
+                    MessageBox.Show("WebView2 is not installed on this system. For more information, visit https://github.com/RyanWalpoleEnterprises/Moxo-QuickWeb");
+                }
+            }
+            catch
+            {
+                //Microsoft Edge WebView2 Runtime Assumed Not Installed
+                MessageBox.Show("WebView2 is not installed on this system. For more information, visit https://github.com/RyanWalpoleEnterprises/Moxo-QuickWeb");
+            }
+        }
+    }
+}
