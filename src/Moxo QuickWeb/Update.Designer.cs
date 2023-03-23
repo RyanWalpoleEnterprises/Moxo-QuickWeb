5be54ee20e3f3902e6f2429cf11d40b1ee7b031a
Ryan Walpole <ryan.walpole@outlook.com>
2023-03-23 11:50:22 +1100

    Handle Launch Arguments
    Implement code to handle launch arguments.
    
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

--- src/Moxo QuickWeb/Update.Designer.cs
+++ src/Moxo QuickWeb/Update.Designer.cs
@@ -82,8 +82,8 @@
             // 
             // Update
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(408, 170);
             this.ControlBox = false;
